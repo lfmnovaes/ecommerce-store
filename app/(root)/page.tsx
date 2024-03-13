648c3fc68f1a0283bfd1a9b703826fa5d0d74e73
Luis Magalhaes <luis.magalhaes@theblueground.com>
2024-03-13 19:59:28 -0300

    First commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import {Button} from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <main className="p-4">
+      <p>Hello Admin Dashboard</p>
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </main>
+  );
+}
