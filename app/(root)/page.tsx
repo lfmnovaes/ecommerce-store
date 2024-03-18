39dafa7ec06d0ffa410cc37bddc492ba50130438
Luis Magalhaes <luis.magalhaes@theblueground.com>
2024-03-18 09:37:14 -0300

    Add effect in button

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ import {Button} from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <main className="p-4">
+    <main className="p-4 h-full text-white bg-black dark space-y-2">
       <p>Hello Admin Dashboard</p>
       <div>
         <Button>Click me</Button>
